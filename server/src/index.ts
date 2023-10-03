a33aca985cc831c2187138dd1a4cf496f21b2f2f
Kevin <kev776522.mg08@nycu.edu.tw>
2023-10-04 00:26:24 +0800

    feat: https

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,6 +1,9 @@
 import express, {NextFunction, Request, Response} from 'express';
 import cors from 'cors';
 import cookieParser from 'cookie-parser';
+import https from 'https';
+import fs from 'fs';
+import path from 'path';
 import {router} from './router';
 const app = express();
 const port = 8000;
@@ -12,7 +15,15 @@ app.use(cors(corsOption));
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 app.use(cookieParser());
+
 app.use('/', router);
+
+app.use(express.static(path.join(__dirname, '../../frontend/build')));
+app.use('/', (_req, res, next) => {
+  res.sendFile(path.join(__dirname, '../../frontend/build/index.html'), err => {
+    next(err);
+  });
+});
 
 app.use(
   (err: Error, _req: Request, res: Response, _next: NextFunction): void => {
@@ -22,6 +33,14 @@ app.use(
   }
 );
 
-app.listen(port, () => {
-  console.log(`App is listening at port ${port}`);
-});
+https
+  .createServer(
+    {
+      key: fs.readFileSync('../ssl/key.pem'),
+      cert: fs.readFileSync('../ssl/certificate.pem'),
+    },
+    app
+  )
+  .listen(port, () => {
+    console.log(`Server is listening at port:${port}`);
+  });
